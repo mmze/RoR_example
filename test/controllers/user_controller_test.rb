8c7e0da77bb03d4378863fee817e4ad87f112dd1
mmze <mmze@yandex.ru>
2018-11-27 09:58:11 +0300

    Make a basic User model (include secure password)

---
+++ test/controllers/user_controller_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class UserControllerTest < ActionDispatch::IntegrationTest
+=begin
+  test "should get name:string" do
+    get user_name:String
+    assert_response :success
+  end
+
+  test "should get email:string" do
+    get user_email:string_url
+    assert_response :success
+  end
+=end
+
+end
