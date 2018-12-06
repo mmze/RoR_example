a3c8bc92e5a72eab04904320853dba2dab8b0e6d
mmze <mmze@yandex.ru>
2018-12-06 15:36:18 +0300

    linux

--- vendor/bundle/ruby/2.5.0/gems/rails-controller-testing-1.0.3/lib/rails/controller/testing/version.rb
+++
@@ -1,7 +0,0 @@
-module Rails
-  module Controller
-    module Testing
-      VERSION = "1.0.3"
-    end
-  end
-end
--- vendor/bundle/ruby/2.5.0/gems/sassc-rails-2.0.0/lib/sassc/rails/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module SassC
-  module Rails
-    VERSION = "2.0.0"
-  end
-end
